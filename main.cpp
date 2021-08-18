3d3a2ed251a7351b36db4f6c041b1bc600245c7b
Utku Çelik <utkuce@gmail.com>
2021-08-18 01:04:14 -0400

    Add subscribing to events on redis

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <fmt/format.h>
 #include <sw/redis++/redis++.h>
 
 using namespace sw::redis;
@@ -8,14 +10,30 @@ int main(int, char**) {
     // Create an Redis object, which is movable but NOT copyable.
     auto redis = Redis("tcp://127.0.0.1:6379");
 
-    // ***** STRING commands *****
+    std::string room_id = "room1";
+    std::string user_id = "user1";
 
-    redis.set("key", "val");
-    auto val = redis.get("key");    // val is of type OptionalString. See 'API Reference' section for details.
-    if (val) {
-        // Dereference val to get the returned value of std::string type.
-        std::cout << *val << std::endl;
-    }   // else key doesn't exist.
+    // for example add user1 to the room1.users list
+    redis.rpush(fmt::format("{}.users", room_id), user_id);
+
+    // Create a Subscriber.
+    auto sub = redis.subscriber();
+
+    sub.on_pmessage([](std::string pattern, std::string channel, std::string msg) {
+        std::cout << pattern << ", " << channel << ", " << msg << std::endl;
+    });
+
+    // Subscribe to channels and patterns.
+    sub.psubscribe("__keyspace@0__:room1.*");
+
+    // Consume messages in a loop.
+    while (true) { // TODO: make seperate thread
+        try {
+            sub.consume();
+        } catch (const Error &err) {
+            std::cout << "Error: " << err.what() << std::endl;
+        }
+    }
 
     return 0;
 }
